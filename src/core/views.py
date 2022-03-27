efff7dfa53fd0dea8380a32a320be3b2f86bd5b5
olfredos6 <alfred.nehemie@gmail.com>
2022-03-27 11:12:25 +0000

    adds and configures handler for 500 error

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request, exception=None):
     ''' Entry for welcome and 404s '''
     return render(request, "index.html")
+
+
+def error(request, exception=None):
+    '''
+        handler for 500 respsonses
+    '''
+    return HttpResponse(
+        f'''<p>Error: {exception}</p>
+        To learn about how to use this API, 
+        head to <a href='https://apis.nehemie.dev/bkg-emailer/welcome'>
+        https://apis.nehemie.dev/bkg-emailer/welcome</a>
+        '''
+    )
